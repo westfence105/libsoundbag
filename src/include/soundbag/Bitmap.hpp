b2697f8a0fae6d1bd8906e920a66f1b61d48135c
Togo Nishigaki <westfence95@gmail.com>
2017-08-20 17:32:35 +0900

    Add SDL_Surface support for Bitmap struct

--- src/include/soundbag/Bitmap.hpp
+++ src/include/soundbag/Bitmap.hpp
@@ -12,15 +12,63 @@
 #define libsoundbag_Bitmap_h
 
 #include <cstdint>
+#include <stdexcept>
 
 #include "soundbag.hpp"
 
 namespace soundbag {
 	struct Bitmap{
-		byte compressed;
+		enum class BitmapFormat {
+			RGB,
+			RGBA,
+		};
+		BitmapFormat format;
 		int32_t width;
 		int32_t height;
 		std::vector<byte> data;
+
+		inline Bitmap(){
+			format = BitmapFormat::RGB;
+			width = height = 0;
+		}
+
+	#ifdef _SDL_H
+		inline Bitmap( SDL_Surface* const s_img ) throw( std::invalid_argument ){
+			if( s_img == NULL ){
+				throw std::invalid_argument("NULL surface given.");
+			}
+
+			width = s_img->w;
+			height = s_img->h;
+
+			if( s_img->format->format == SDL_PIXELFORMAT_RGB888 ||
+				s_img->format->format == SDL_PIXELFORMAT_RGB24 ){
+				format = BitmapFormat::RGB;
+				std::vector<byte>( (byte*)s_img->pixels, (byte*)s_img->pixels + ( width * height * 3 ) ).swap( data );
+
+			}
+			else if( s_img->format->format == SDL_PIXELFORMAT_RGBA8888 ){
+				format = BitmapFormat::RGBA;
+				std::vector<byte>( (byte*)s_img->pixels, (byte*)s_img->pixels + ( width * height * 3 ) ).swap( data );
+			}
+			else {
+				format = BitmapFormat::RGBA;
+
+				SDL_PixelFormat s_fmt;
+				s_fmt.format = SDL_PIXELFORMAT_RGBA8888;
+				s_fmt.palette = NULL;
+				s_fmt.BitsPerPixel = 32;
+				s_fmt.BytesPerPixel = 4;
+				s_fmt.Rmask = 0xFF000000;
+				s_fmt.Gmask = 0x00FF0000;
+				s_fmt.Bmask = 0x0000FF00;
+				s_fmt.Amask = 0x000000FF;
+				SDL_Surface* c_surface = SDL_ConvertSurface( s_img, &s_fmt, 0 );
+				std::vector<byte>( (byte*)c_surface->pixels, (byte*)c_surface->pixels + ( width * height * 4 ) ).swap( data );
+				SDL_FreeSurface( c_surface );
+			}
+		}
+	#endif
 	};
 }
 
--- test/SBGL_test.cpp
+++ test/SBGL_test.cpp
@@ -1,11 +1,37 @@
 #include "gtest/gtest.h"
 
+#include <memory>
+#include <deque>
+
+#include <GL/glew.h>
+
 #include "soundbag/SDL_GL_Window.hpp"
 #include "soundbag/SBGL.hpp"
+#include "soundbag/SBGL_Object.hpp"
 #include "soundbag/SBGL_Dialog.hpp"
 #include "soundbag/SBGL_Text.hpp"
 
 using namespace soundbag;
+
+class SBGL_TestWindow : SDL_GL_Window {
+	std::deque<std::string>& errors;
+
+	std::shared_ptr<SBGL_Object> test_obj;
+
+public:
+	SBGL_TestWindow( std::deque<std::string>& errors );
+	~SBGL_TestWindow();
+
+	void prepare();
+	void draw();
+
+};
+
+int main( int argc, char** argv ){
+	testing::InitGoogleTest( &argc, argv );
+
+	return RUN_ALL_TESTS();
+}
 
 TEST( SBGL, push_coord ){
 	std::vector<GLfloat> v;
@@ -35,4 +61,22 @@ TEST( SBGL, nextPow2 ){
 	EXPECT_EQ( nextPow2(  2 ),  2 );
 	EXPECT_EQ( nextPow2(  3 ),  4 );
 	EXPECT_EQ( nextPow2( 12 ), 16 );
+}
+
+TEST( SBGL, drawing ){
+	std::deque<std::string> errors;
+
+	auto window = std::make_shared<SBGL_TestWindow>()
+
+	auto ret = glewInit();
+	if( ret != GLEW_OK ){
+		std::cerr << "GLEW Error: " << glewGetErrorString(ret) << std::endl;
+		return 1;
+	}
+
+	window->main();
+}
+
+TEST( SBGL, SBGL_Object_instance ){
+	auto obj = std::make_shared<SBGL_Object>();
 }
